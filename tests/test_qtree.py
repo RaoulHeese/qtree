2e9cb2250e640fcbff9457166c87b2e93fd7b832
RaoulHeese <raoul.heese@gmail.com>
2022-09-08 22:30:19 +0200

    +test: mwe

---
+++ tests/test_qtree.py
@@ -0,0 +1,16 @@
+"""Tests: Quantum trees.
+"""
+import pytest
+import numpy as np
+from qtree.qtree import QTree
+
+    
+def test_mwe():
+    """Minimal working example from readme.
+    """
+    qtree = QTree(max_depth=1)
+    X = np.array([[1,0,0], [0,1,0], [0,0,1]])
+    y = np.array([[0,0], [0,1], [1,1]])
+    qtree.fit(X, y)
+    qtree.predict([[0,0,1]])
+    
